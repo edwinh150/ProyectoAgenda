fdbe4de9c72b87dd6292593c0527e177e42d7670
edwinh150 <edwinh2410@gmail.com>
2016-03-06 21:37:33 -0400

    subiendo lo que falta

--- BLL/Destinos.cs
+++ BLL/Destinos.cs
@@ -10,7 +10,7 @@ namespace BLL
     public class Destinos : ClaseMaestra
     {
         public int DestinoId { get; set; }
-        public int PaisId { get; set; }
+        public string Descripcion { get; set; }
         public int CiudadId { get; set; }
 
 
@@ -21,7 +21,7 @@ namespace BLL
 
             try
             {
-                retorno = Conexion.Ejecutar(string.Format("insert into Usuarios(NombreUsuario,Contrasena,Nombre,Apellido,Email,Telefono,TipoUsuarioId) values('{0}','{1}','{2}','{3}','{4}','{5}',{6}) ", this.CiudadId));
+                retorno = Conexion.Ejecutar(string.Format("insert into Destinos(Descripcion,CiudadId) values('{0}',{1}) ", this.Descripcion, this.CiudadId));
             }
             catch (Exception)
             {
@@ -38,7 +38,7 @@ namespace BLL
 
             try
             {
-                retorno = Conexion.Ejecutar(string.Format("update Usuarios set NombreUsuario = '{0}',Contrasena = '{1}', Nombre = '{2}',Apellido = '{3}', Email = '{4}', Telefono = '{5}', TipoUsuarioId = {6} from Usuarios where UsuarioId = {7}", this.PaisId));
+                retorno = Conexion.Ejecutar(string.Format("update Destinos set Descripcion = '{0}', CiudadId = {1} where DestinoId = {2}", this.DestinoId));
             }
             catch (Exception)
             {
@@ -55,7 +55,7 @@ namespace BLL
 
             try
             {
-                retorno = Conexion.Ejecutar(string.Format("delete from Usuarios where UsuarioId = {0}", this.DestinoId));
+                retorno = Conexion.Ejecutar(string.Format("delete from Destinos where DestinoId = {0}", this.DestinoId));
             }
             catch (Exception)
             {
--- BLL/Viajes.cs
+++ BLL/Viajes.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -9,30 +10,114 @@ namespace BLL
     public class Viajes : ClaseMaestra
     {
         public int ViajeId { get; set; }
+        public int OrigenId { get; set; }
+        public int DestinoId { get; set; }
+        public DateTime FechaIncial { get; set; }
+        public DateTime FechaFinal { get; set; }
+        public int CantidadPersona { get; set; }
+        public int CantidadNino { get; set; }
+        public string EdadesNino { get; set; }
+        public double PrecioInicial { get; set; }
+        public double PrecioFinal { get; set; }
 
-        public override bool Buscar(int IdBuscado)
+        public override bool Insertar()
         {
-            throw new NotImplementedException();
+            bool retorno = false;
+            ConexionDB Conexion = new ConexionDB();
+
+            try
+            {
+                retorno = Conexion.Ejecutar(string.Format("insert into Viajes(OrigenId,DestinoId,FechaInicial,FechaFinal,CantidadPersona,CantidadNino,EdadesNino,PrecioInicial,PrecioFinal) values({0},{1},'{2}','{3}',{4},{5},'{6}',{7},{8}) ", this.OrigenId, this.DestinoId, this.FechaIncial, this.FechaFinal, this.CantidadPersona, this.CantidadNino, this.EdadesNino, this.FechaIncial, this.FechaFinal));
+            }
+            catch (Exception)
+            {
+                retorno = false;
+            }
+
+            return retorno;
         }
 
         public override bool Editar()
         {
-            throw new NotImplementedException();
+            bool retorno = false;
+            ConexionDB Conexion = new ConexionDB();
+
+            try
+            {
+                retorno = Conexion.Ejecutar(string.Format("Update Viajes set OrigenId = {0}, DestinoId = {1}, FechaInicial = {2}, FechaFinal = {3}, CantidadPersona = {4}, CantidadNino = {5}, EdadesNino = {6}, PrecioInicial = {7}, PrecioFinal = {8} where ViajeId = {9}", this.OrigenId, this.DestinoId, this.FechaIncial, this.FechaFinal, this.CantidadPersona, this.CantidadNino, this.EdadesNino, this.FechaIncial, this.FechaFinal, this.ViajeId));
+            }
+            catch (Exception)
+            {
+                retorno = false;
+            }
+
+            return retorno;
         }
 
         public override bool Eliminar()
         {
-            throw new NotImplementedException();
+            bool retorno = false;
+            ConexionDB Conexion = new ConexionDB();
+
+            try
+            {
+                retorno = Conexion.Ejecutar(string.Format("delete from Viajes where ViajeId = {0}", this.ViajeId));
+            }
+            catch (Exception)
+            {
+
+                retorno = false;
+            }
+
+            return retorno;
         }
 
-        public override bool Insertar()
+        public override bool Buscar(int IdBuscado)
         {
-            throw new NotImplementedException();
+            bool retorno = false;
+            DataTable dt = new DataTable();
+            ConexionDB Conexion = new ConexionDB();
+
+            try
+            {
+                dt = Conexion.ObtenerDatos(string.Format("select * from Viajes where ViajeId = {0} ", IdBuscado));
+
+                if (dt.Rows.Count > 0)
+                {
+                    this.OrigenId = (int)dt.Rows[0]["OrigenId"];
+                    this.DestinoId = (int)dt.Rows[0]["DestinoId"];
+                    this.ViajeId = (int)dt.Rows[0]["ViajeId"];
+                    this.FechaIncial = (DateTime)dt.Rows[0]["FechaIncial"];
+                    this.FechaFinal = (DateTime)dt.Rows[0]["FechaFinal"];
+                    this.CantidadPersona = (int)dt.Rows[0]["CantidadPersona"];
+                    this.CantidadNino = (int)dt.Rows[0]["CantidadNino"];
+                    this.EdadesNino = dt.Rows[0]["EdadesNino"].ToString();
+                    this.PrecioInicial = (double)dt.Rows[0]["PrecioInicial"];
+                    this.PrecioFinal = (double)dt.Rows[0]["PrecioFinal"];
+                    retorno = true;
+                }
+            }
+            catch (Exception)
+            {
+                retorno = false;
+            }
+
+            return retorno;
         }
 
         public override DataTable Listado(string Campos, string Condicion, string Orden)
         {
-            throw new NotImplementedException();
+            ConexionDB Conexion = new ConexionDB();
+
+            try
+            {
+                return Conexion.ObtenerDatos(string.Format("select " + Campos + " From Viajes where " + Condicion + " " + Orden));
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
     }
 }
