ea13c0739bc9d18a377b6323d91c36d873335542
edwinh150 <edwinh2410@gmail.com>
2016-03-06 21:57:36 -0400

    agregando crucero

--- BLL/Cruceros.cs
+++ BLL/Cruceros.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -9,30 +10,103 @@ namespace BLL
     public class Cruceros : ClaseMaestra
     {
         public int CruceroId { get; set; }
-        
-        public override bool Buscar(int IdBuscado)
+        public int UsuarioId { get; set; }
+        public int ViajeId { get; set; }
+        public int CategoriaCruceroId { get; set; }
+        public int CompaniaCruceroId { get; set; }
+
+        public override bool Insertar()
         {
-            throw new NotImplementedException();
+            bool retorno = false;
+            ConexionDB Conexion = new ConexionDB();
+
+            try
+            {
+                retorno = Conexion.Ejecutar(string.Format("insert into Cruceros(UsuarioId,ViajeId,CategoriaCruceroId,CompaniaCruceroId) values({0},{1},{2},{3}) ", this.UsuarioId, this.ViajeId, this.CategoriaCruceroId, this.CompaniaCruceroId));
+            }
+            catch (Exception)
+            {
+                retorno = false;
+            }
+
+            return retorno;
         }
 
         public override bool Editar()
         {
-            throw new NotImplementedException();
+            bool retorno = false;
+            ConexionDB Conexion = new ConexionDB();
+
+            try
+            {
+                retorno = Conexion.Ejecutar(string.Format("update Cruceros set UsuarioId = {0}, ViajeId = {1}, CategoriaCruceroId = {2}, CompaniaCruceroId = {3} where CruceroId = {4}", this.UsuarioId, this.ViajeId, this.CategoriaCruceroId, this.CompaniaCruceroId, this.CruceroId));
+            }
+            catch (Exception)
+            {
+                retorno = false;
+            }
+
+            return retorno;
         }
 
         public override bool Eliminar()
         {
-            throw new NotImplementedException();
+            bool retorno = false;
+            ConexionDB Conexion = new ConexionDB();
+
+            try
+            {
+                retorno = Conexion.Ejecutar(string.Format("delete from Cruceros where CruceroId = {0}", this.CruceroId));
+            }
+            catch (Exception)
+            {
+
+                retorno = false;
+            }
+
+            return retorno;
         }
 
-        public override bool Insertar()
+        public override bool Buscar(int IdBuscado)
         {
-            throw new NotImplementedException();
+            bool retorno = false;
+            DataTable dt = new DataTable();
+            ConexionDB Conexion = new ConexionDB();
+
+            try
+            {
+                dt = Conexion.ObtenerDatos(string.Format("select * from Cruceros where CruceroId = {0} ", IdBuscado));
+
+                if (dt.Rows.Count > 0)
+                {
+                    this.UsuarioId = (int)dt.Rows[0]["UsuarioId"];
+                    this.ViajeId = (int)dt.Rows[0]["ViajeId"];
+                    this.CategoriaCruceroId = (int)dt.Rows[0]["CategoriaCruceroId"];
+                    this.CompaniaCruceroId = (int)dt.Rows[0]["CompaniaCruceroId"];
+                    retorno = true;
+                }
+            }
+            catch (Exception)
+            {
+                retorno = false;
+            }
+
+            return retorno;
         }
 
         public override DataTable Listado(string Campos, string Condicion, string Orden)
         {
-            throw new NotImplementedException();
+            ConexionDB Conexion = new ConexionDB();
+
+            try
+            {
+                return Conexion.ObtenerDatos(string.Format("select " + Campos + " From Cruceros where " + Condicion + " " + Orden));
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
     }
 }
