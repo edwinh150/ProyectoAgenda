f96ef94730b73cdf1e87d2a4ba70a400dce347df
Edwin Hidalgo Lopez <edwin_hidalgo1@ucne.edu.do>
2016-03-07 22:27:22 -0400

    subiendo uno excluido

---
+++ ProyectoAgencia/Registros/rCategoriaAerolineas.aspx.cs
@@ -0,0 +1,153 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProyectoAgencia.Registros
+{
+    public partial class rCategoriaAerolineas : System.Web.UI.Page
+    {
+        CategoriaAerolineas CategoriaAerolinea = new CategoriaAerolineas();
+        Seguridad Seguro = new Seguridad();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void ValidacionLimpiar()
+        {
+            RequiredFieldValidator1.IsValid = true;
+        }
+
+        public void Limpiar()
+        {
+            DescripcionTextBox.Text = "";
+            ValidacionLimpiar();
+
+        }
+
+        bool LLenarDatos()
+        {
+            bool retorno = false;
+
+            if (DescripcionTextBox.Text.Length > 0)
+            {
+                CategoriaAerolinea.Descripcion = DescripcionTextBox.Text;
+                retorno = true;
+            }
+            else
+            {
+                retorno = false;
+            }
+
+            return retorno;
+        }
+
+        protected void GuardarButton_Click(object sender, EventArgs e)
+        {
+            if (CategoriaAerolineaIdTextBox.Text.Length > 0)
+            {
+                if (LLenarDatos())
+                {
+                    CategoriaAerolinea.CategoriaAerolineaId = Seguro.ValidarEntero(CategoriaAerolineaIdTextBox.Text);
+
+                    if (CategoriaAerolinea.Editar())
+                    {
+                        Mensajes.ShowToastr(this.Page, "Se Modifico", "Informacion", "Success");
+                        Limpiar();
+                    }
+                    else
+                    {
+                        Mensajes.ShowToastr(this.Page, "No Se Modifico", "Error", "Error");
+                    }
+
+                }
+                else
+                {
+                    Mensajes.ShowToastr(this.Page, "Faltan Datos", "Error", "Error");
+                }
+            }
+            else
+            {
+                if (LLenarDatos())
+                {
+                    if (CategoriaAerolinea.Insertar())
+                    {
+                        Mensajes.ShowToastr(this.Page, "Se Registro", "Felicidades", "Success");
+                        Limpiar();
+                    }
+                    else
+                    {
+                        Mensajes.ShowToastr(this.Page, "No se pudo Registrar", "Error", "Error");
+                    }
+                }
+                else
+                {
+                    Mensajes.ShowToastr(this.Page, "Faltan Datos", "Error", "Error");
+                }
+            }
+        }
+
+        protected void EliminarButton_Click(object sender, EventArgs e)
+        {
+            if (CategoriaAerolineaIdTextBox.Text.Length > 0)
+            {
+                if (Seguro.ValidarEntero(CategoriaAerolineaIdTextBox.Text) > 0)
+                {
+                    CategoriaAerolinea.CategoriaAerolineaId = Seguro.ValidarEntero(CategoriaAerolineaIdTextBox.Text);
+
+                    if (CategoriaAerolinea.Eliminar())
+                    {
+                        Mensajes.ShowToastr(this.Page, "Se Guardo", "Informacion", "Success");
+                        Limpiar();
+                    }
+                    else
+                    {
+                        Mensajes.ShowToastr(this.Page, "No Se Elimino", "Error", "Error");
+                    }
+                }
+                else
+                {
+                    Mensajes.ShowToastr(this.Page, "No hay Registro", "Error", "Error");
+                    Limpiar();
+                }
+            }
+            else
+            {
+                Mensajes.ShowToastr(this.Page, "Ingrese un Id valido primero", "Error", "Error");
+            }
+        }
+
+        protected void BuscarButton_Click(object sender, EventArgs e)
+        {
+            int Id = Seguro.ValidarEntero(CategoriaAerolineaIdTextBox.Text);
+            ValidacionLimpiar();
+
+            if (Id > 0)
+            {
+                if (CategoriaAerolinea.Buscar(Id))
+                {
+                    DescripcionTextBox.Text = CategoriaAerolinea.Descripcion;
+                }
+                else
+                {
+                    Mensajes.ShowToastr(this.Page, "No hay Registro", "Informacion", "info");
+                    Limpiar();
+                }
+            }
+            else
+            {
+                Mensajes.ShowToastr(this.Page, "Ingrese un Id valido primero", "Error", "Error");
+            }
+        }
+
+        protected void LimpiarButton_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+    }
+}
