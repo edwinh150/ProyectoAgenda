51f105a7fcb209db8e469211a22a2004e90d7cb1
edwinh150 <edwinh2410@gmail.com>
2016-03-27 22:05:47 -0400

    arreglando referencia mala

--- ProyectoAgencia/Site.Master.designer.cs
+++ ProyectoAgencia/Site.Master.designer.cs
@@ -119,14 +119,5 @@ namespace ProyectoAgencia {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
-        
-        /// <summary>
-        /// Label1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
     }
 }
